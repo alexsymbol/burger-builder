ca399b9fa8aa9b92707d282c7d8b34d7d9d0b53d
Alex Symbol <simvol710@gmail.com>
2019-03-06 19:23:13 +0200

    starting implementation

---
+++ src/containers/BurgerBuilder/BurgerBilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class BurgerBilder extends Component {
+	render () {
+		return (
+			<Aux>
+				<div>Burger</div>
+				<div>Build Controls</div>
+			</Aux>
+		);
+	}
+}
+
+export default BurgerBilder;
